5461f61c3e5cc670a406717ffbb77b89e62d0811
juan <juan@Juans-MacBook-Pro.local>
2018-11-09 10:55:54 -0300

    initial

--- 1.2_vsearch.sh
+++ 1.2_vsearch.sh
@@ -5,12 +5,11 @@ mkdir data/results/clusters_mites/
 grep ">" data/results/mites.fasta | wc -l
 4074
 
-./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/fasta/mites.fasta --threads 10 --strand both  --clusters data/results/clusters_mites/c --iddef 1 -id 0.8
-
+./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/mites.fasta --threads 10 --strand both  --clusters data/results/clusters_mites/c --iddef 1 -id 0.8
 vsearch v2.9.1_linux_x86_64, 16.0GB RAM, 16 cores
 https://github.com/torognes/vsearch
 
-Reading file data/fasta/mites.fasta 100%  
+Reading file data/results/mites.fasta 100%  
 1397169 nt in 4074 seqs, min 50, max 902, avg 343
 Masking 100%  
 Sorting by length 100%
@@ -19,7 +18,7 @@ Clustering 100%
 Sorting clusters 100%
 Writing clusters 100%  
 Clusters: 2542 Size min 1, max 84, avg 1.6
-Singletons: 2174, 53.4% of seqs, 85.5% of cluster
+Singletons: 2174, 53.4% of seqs, 85.5% of clusters
 
 
 cd data/results
