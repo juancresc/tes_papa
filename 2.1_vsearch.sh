4cb389ac40c78a50de68141a338f5b881a80ce6e
juan <juan@Juans-MacBook-Pro.local>
2018-12-03 11:12:08 -0300

    init

--- 2.1_vsearch.sh
+++ 2.1_vsearch.sh
@@ -3,18 +3,18 @@ rm -r data/results/clusters_line/ 2> /dev/null
 mkdir data/results/clusters_line/
 rm -r data/results/clusters_sine/ 2> /dev/null
 mkdir data/results/clusters_sine/
-rm -r data/results/clusters_mites/ 2> /dev/null
-mkdir data/results/clusters_mites/
+rm -r data/results/clusters_mite/ 2> /dev/null
+mkdir data/results/clusters_mite/
 rm -r data/results/clusters_ltr/ 2> /dev/null
 mkdir data/results/clusters_ltr/
-rm -r data/results/clusters_helitrons/ 2> /dev/null
-mkdir data/results/clusters_helitrons/
-rm -r data/results/clusters_tirs/ 2> /dev/null
-mkdir data/results/clusters_tirs/
+rm -r data/results/clusters_helitron/ 2> /dev/null
+mkdir data/results/clusters_helitron/
+rm -r data/results/clusters_tir/ 2> /dev/null
+mkdir data/results/clusters_tir/
 rm -r data/results/clusters_trims/ 2> /dev/null
 mkdir data/results/clusters_trims/
-rm -r data/results/clusters_lards/ 2> /dev/null
-mkdir data/results/clusters_lards/
+rm -r data/results/clusters_lard/ 2> /dev/null
+mkdir data/results/clusters_lard/
 
 
 #LINE
@@ -23,8 +23,8 @@ nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_line.fast
 
 
 #TIR
-grep ">" data/results/seqs_tirs.fasta | wc -l
-nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_tirs.fasta --threads 2 --strand both  --clusters data/results/clusters_tir/c --iddef 1 -id 0.9 &
+grep ">" data/results/seqs_tir.fasta | wc -l
+nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_tir.fasta --threads 2 --strand both  --clusters data/results/clusters_tir/c --iddef 1 -id 0.9 &
 
 #SINE
 grep ">" data/results/seqs_sine.fasta | wc -l
@@ -35,30 +35,30 @@ grep ">" data/results/seqs_ltr.fasta | wc -l
 nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_ltr.fasta --threads 2 --strand both  --clusters data/results/clusters_ltr/c --iddef 1 -id 0.9 &
 
 #Helitrons
-grep ">" data/results/seqs_helitrons.fasta | wc -l
-nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_helitrons.fasta --threads 2 --strand both  --clusters data/results/clusters_helitrons/c --iddef 1 -id 0.9 & 
+grep ">" data/results/seqs_helitron.fasta | wc -l
+nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_helitron.fasta --threads 2 --strand both  --clusters data/results/clusters_helitron/c --iddef 1 -id 0.9 & 
 
 #TIRs
-grep ">" data/results/seqs_tirs.fasta | wc -l
-nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_tirs.fasta --threads 2 --strand both  --clusters data/results/clusters_tirs/c --iddef 1 -id 0.9 &
+grep ">" data/results/seqs_tir.fasta | wc -l
+nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_tir.fasta --threads 2 --strand both  --clusters data/results/clusters_tir/c --iddef 1 -id 0.9 &
 
 #LARDs
-grep ">" data/results/seqs_lards.fasta | wc -l
-nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_lards.fasta --threads 2 --strand both  --clusters data/results/clusters_lards/c --iddef 1 -id 0.9 &
+grep ">" data/results/seqs_lard.fasta | wc -l
+nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_lard.fasta --threads 2 --strand both  --clusters data/results/clusters_lard/c --iddef 1 -id 0.9 &
 
-#TRIMs
+#TRIM
 grep ">" data/results/seqs_trim.fasta | wc -l
 nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_trim.fasta --threads 2 --strand both  --clusters data/results/clusters_trim/c --iddef 1 -id 0.9 &
 
 #MITEs 
-grep ">" data/results/seqs_mites.fasta | wc -l
-nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_mites.fasta --threads 2 --strand both  --clusters data/results/clusters_mites/c --iddef 1 -id 0.9 &
+grep ">" data/results/seqs_mite.fasta | wc -l
+nohup ./sw/vsearch-2.9.1/bin/vsearch --cluster_fast  data/results/seqs_mite.fasta --threads 2 --strand both  --clusters data/results/clusters_mite/c --iddef 1 -id 0.9 &
 
 cd data/results
 tar -zcvf clusters_ltr.tar.gz clusters_ltr/
-tar -zcvf clusters_mites.tar.gz clusters_mites/
+tar -zcvf clusters_mite.tar.gz clusters_mite/
 tar -zcvf clusters_line.tar.gz clusters_line/
-tar -zcvf clusters_helitrons.tar.gz clusters_helitrons/
+tar -zcvf clusters_helitron.tar.gz clusters_helitron/
 tar -zcvf clusters_lard.tar.gz clusters_lard/
 tar -zcvf clusters_sine.tar.gz clusters_sine/
-tar -zcvf clusters_tirs.tar.gz clusters_tirs/
+tar -zcvf clusters_tir.tar.gz clusters_tir/
