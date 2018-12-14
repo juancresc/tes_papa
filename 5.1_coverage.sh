79939569c275c92de5e36548704fdcd86170a677
juan <juan@Juans-MacBook-Pro.local>
2018-12-14 15:57:31 -0300

    init

--- 5.1_coverage.sh
+++ 5.1_coverage.sh
@@ -8,6 +8,17 @@ grep "LTR" data/results/all.gff > data/results/all_ltr.gff
 grep "helitron" data/results/all.gff > data/results/all_helitron.gff
 grep "TIR" data/results/all.gff > data/results/all_tir.gff
 
+grep "DTT" data/results/all.gff > data/results/all_dtt.gff
+grep "DTA" data/results/all.gff > data/results/all_dta.gff
+grep "DTH" data/results/all.gff > data/results/all_dth.gff
+grep "DTX" data/results/all.gff > data/results/all_dtx.gff
+
+
+python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_dtt.gff >> data/results/coverage.txt
+python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_dta.gff >> data/results/coverage.txt
+python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_dth.gff >> data/results/coverage.txt
+python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_dtx.gff >> data/results/coverage.txt
+
 python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all.gff >> data/results/coverage.txt
 python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_trim.gff >> data/results/coverage.txt
 python gff2coverage.py -r data/Stuberosum_genome.gff3 -a data/results/all_line.gff >> data/results/coverage.txt
