3a2b9c0dd9c003e0cf78639403bfcf25bb121c24
juan <juan@Juans-MacBook-Pro.local>
2019-02-15 13:56:11 -0300

    init

--- 5.1_coverage.sh
+++ 5.1_coverage.sh
@@ -1,4 +1,4 @@
-  awk '{ if ($1 == "ChrUn") { print } }' data/results/all.gff > data/results/all.noun.gff
+awk '{ if ($1 != "ChrUn") { print } }' data/results/all.gff > data/results/all.noun.gff
 echo "" > data/results/coverage.txt
 grep "TRIM_id" data/results/all.noun.gff > data/results/all_trim.gff
 grep "LINE_id" data/results/all.noun.gff > data/results/all_line.gff
@@ -9,9 +9,9 @@ grep "LTR_id" data/results/all.noun.gff > data/results/all_ltr.gff
 grep "helitron_id" data/results/all.noun.gff > data/results/all_helitron.gff
 grep "TIR_id" data/results/all.noun.gff > data/results/all_tir.gff
 
-grep "RLG" data/results/all.noun.gff > data/results/all_rlg.gff
-grep "RLC" data/results/all.noun.gff > data/results/all_rlc.gff
-grep "RLX" data/results/all.noun.gff > data/results/all_rlx.gff
+grep "RLG" data/results/all_ltr.gff > data/results/all_rlg.gff
+grep "RLC" data/results/all_ltr.gff > data/results/all_rlc.gff
+grep "RLX" data/results/all_ltr.gff > data/results/all_rlx.gff
 grep "DTM" data/results/all.noun.gff > data/results/all_dtm.gff
 grep "DTC" data/results/all.noun.gff > data/results/all_dtc.gff
 grep "DTT" data/results/all.noun.gff > data/results/all_dtt.gff
