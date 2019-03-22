ece82638d84f44941221ca408098133155e8965d
Juan Manuel Crescente <you@example.com>
2019-03-22 10:26:48 -0300

    dasdsa

--- 8_2age.R
+++ 8_2age.R
@@ -4,9 +4,9 @@ devtools::install_github('TickingClock1992/RIdeogram')
 
 require(RIdeogram)
 
-setwd("/Users/juan/Documents/manu/dev/vms/bio/TEs_papa/data/")
-#data(human_karyotype, package="RIdeogram")
-#data(gene_density, package="RIdeogram")
+setwd("/home/juan/Desktop/juan/bio/tes_papa/data")
+data(human_karyotype, package="RIdeogram")
+data(gene_density, package="RIdeogram")
 #data(Random_RNAs_500, package="RIdeogram")
 potato_karyotype <- read.table("karyotype.txt", sep = ",", header = T, stringsAsFactors = F)
 head(potato_karyotype)
@@ -16,5 +16,3 @@ head(gene_density)
 
 ideogram(karyotype = human_karyotype, overlaid = gene_density)
 convertSVG("chromosome.svg", device = "png")
-
-getwd()
