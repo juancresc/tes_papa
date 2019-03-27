e7bb973b5fc39af59dcb736bb06e3120c8473424
Juan Manuel Crescente <you@example.com>
2019-03-27 10:37:03 -0300

    dasdsa

--- 8_2age.R
+++ 8_2age.R
@@ -7,14 +7,18 @@ require(RIdeogram)
 setwd("/home/juan/Desktop/juan/bio/tes_papa/data")
 #data(human_karyotype, package="RIdeogram")
 data(gene_density, package="RIdeogram")
-gene_density
 #data(Random_RNAs_500, package="RIdeogram")
 potato_karyotype <- read.table("karyotype.csv",sep="\t", header = T, stringsAsFactors = F)
 potato_karyotype$CE_start = potato_karyotype$CE_start - 1000000
 potato_karyotype$CE_end = potato_karyotype$CE_end + 1000000
 head(potato_karyotype)
 
-potato_gene_density <- read.table("ltrs_ages.csv", sep = "\t", header = T, stringsAsFactors = F)
+potato_gene_density <- read.table("ltr_ages.csv", sep = "\t", header = T, stringsAsFactors = F)
+nrow(potato_gene_density)
+head(potato_gene_density)
+potato_gene_density = potato_gene_density[complete.cases(potato_gene_density), ]
+nrow(potato_gene_density)
+
 head(potato_gene_density)
 
 ideogram(karyotype = potato_karyotype, overlaid = potato_gene_density)
