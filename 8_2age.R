376e90dcd935725755e22874449e69be0285fa58
juan <juan@Juans-MacBook-Pro.local>
2019-04-11 08:37:10 -0300

    table css fixes

--- 8_2age.R
+++ 8_2age.R
@@ -4,7 +4,7 @@ devtools::install_github('TickingClock1992/RIdeogram')
 
 require(RIdeogram)
 
-setwd("/home/juan/Desktop/juan/bio/tes_papa/data")
+setwd("/Users/juan/Documents/manu/dev/vms/bio/TEs_papa/data")
 #data(human_karyotype, package="RIdeogram")
 data(gene_density, package="RIdeogram")
 #data(Random_RNAs_500, package="RIdeogram")
@@ -13,7 +13,7 @@ potato_karyotype$CE_start = potato_karyotype$CE_start - 1000000
 potato_karyotype$CE_end = potato_karyotype$CE_end + 1000000
 head(potato_karyotype)
 
-potato_gene_density <- read.table("ltr_ages.csv", sep = "\t", header = T, stringsAsFactors = F)
+potato_gene_density <- read.table("lrx_ages.csv", sep = "\t", header = T, stringsAsFactors = F)
 nrow(potato_gene_density)
 head(potato_gene_density)
 potato_gene_density = potato_gene_density[complete.cases(potato_gene_density), ]
